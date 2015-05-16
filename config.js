ff5cf173b09cbd165aa085afe1108a5d72d6bcbe
Hans van den Akker <hans@bizboard.nl>
2015-05-16 10:47:53 +0200

    jspm 0.15 compliant.

--- config.js
+++ config.js
@@ -11,18 +11,28 @@ System.config({
   "map": {
     "arva-context": "github:Bizboard/arva-context@master",
     "arva-ds": "github:Bizboard/arva-ds@master",
-    "di.js": "github:angular/di.js@master",
+    "di.js": "github:Bizboard/arva-mvc@master",
     "famous": "npm:famous@0.3.5",
     "famous-bkimagesurface": "github:ijzerenhein/famous-bkimagesurface@1.0.3",
-    "famous-flex": "github:ijzerenhein/famous-flex@0.3.1",
+    "famous-flex": "github:ijzerenhein/famous-flex@0.3.2",
     "firebase": "github:firebase/firebase-bower@2.2.4",
-    "lodash": "npm:lodash@3.7.0",
+    "lodash": "npm:lodash@3.8.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "underscore": "npm:underscore@1.8.3",
     "github:Bizboard/arva-ds@master": {
       "arva-context": "github:Bizboard/arva-context@master",
       "di.js": "github:angular/di.js@master",
+      "firebase": "github:firebase/firebase-bower@2.2.4",
+      "lodash": "npm:lodash@3.8.0"
+    },
+    "github:Bizboard/arva-mvc@master": {
+      "arva-context": "github:Bizboard/arva-context@master",
+      "arva-ds": "github:Bizboard/arva-ds@master",
+      "di.js": "github:angular/di.js@master",
+      "famous": "npm:famous@0.3.5",
+      "famous-bkimagesurface": "github:ijzerenhein/famous-bkimagesurface@1.0.3",
+      "famous-flex": "github:ijzerenhein/famous-flex@0.3.2",
       "firebase": "github:firebase/firebase-bower@2.2.4",
       "lodash": "npm:lodash@3.8.0"
     },
@@ -90,9 +100,6 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:lodash@3.7.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:lodash@3.8.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
