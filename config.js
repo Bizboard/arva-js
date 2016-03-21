dc78c619a36ff0756ffd02e6eb89ced73899f151
Manuel Overdijk <manuel.overdijk@gmail.com>
2016-03-21 11:22:16 +0100

    Changed paths to relative paths

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ System.config({
     ]
   },
   paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
+    "github:*": "./jspm_packages/github/*",
+    "npm:*": "./jspm_packages/npm/*"
   },
 
   map: {
