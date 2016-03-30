3a0a42cd5aa67df31848d45fda346f44d2e35a12
Karl Lundfall <karl.lundfall@bizboard.nl>
2016-03-30 12:45:56 +0200

    Added extended tests to core classes

--- test/core/ControllerSpec.js
+++ test/core/ControllerSpec.js
@@ -1,7 +1,9 @@
+import sinon                        from 'sinon';
 import chai                         from 'chai';
-import {loadDependencies,
+import {loadDependencies,mockDOMGlobals,restoreDOMGlobals,restoreDependency,
     mockDependency}                 from '../meta/TestBootstrap.js';
 
+let expect = chai.expect;
 let should = chai.should();
 
 describe('Controller', () => {
@@ -10,7 +12,6 @@ describe('Controller', () => {
     before(() => {
         /* Mock famous-flex's FlexScrollView so no attempt to insert anything into the DOM is made. */
         mockDependency('famous-flex/AnimationController.js', System.newModule({default: function () { this.options = {}; }}));
-
         return loadDependencies({
             Controller: System.normalizeSync('./src/core/Controller.js')
         }).then((importedObjects) => {
@@ -19,13 +20,22 @@ describe('Controller', () => {
     });
 
     after(() => {
-        System.delete(System.normalizeSync('famous-flex/AnimationController.js'));
+        restoreDependency('famous-flex/AnimationController.js');
     });
 
     describe('#constructor', () => {
         it('constructs without exceptions', () => {
-            let instance = new imports.Controller({add: () => {}});
+            let instance = new imports.Controller({add: sinon.stub()});
             should.exist(instance);
+        });
+    });
+
+    describe('#router path', () => {
+        it('sets the router path correctly', () => {
+            let addFunction = sinon.spy();
+            class MyCustomController extends imports.Controller {};
+            let instance = new MyCustomController({add: addFunction});
+            expect(addFunction.withArgs('MyCustom/:method',instance.onRouteCalled).calledOnce).to.be.true;
         });
     });
 });
