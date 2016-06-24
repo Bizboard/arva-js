d70bc513a264b5ecbaee327d053b14293452248e
Karl Lundfall <karl@bizboard.nl>
2016-06-24 13:46:40 +0200

    fixed storage

--- webworker/Worker.js
+++ webworker/Worker.js
@@ -10,20 +10,40 @@ var handler = {
 };
 
 
+
+
 window={};
 self.window = new Proxy(self.window, handler);
 
 importScripts("./firebase.js");
 
 self.window = self;
-self.window.localStorage = {
-    getItem: function(){
-        console.log('getItem');
-    },
-    setItem: function(){
-        console.log('setItem');
+
+class Storage {
+
+    constructor(storageObject) {
+        this.storageObject = storageObject;
     }
-};
+    getItem(key){
+        return this.storageObject[key];
+    }
+    setItem(key, value){
+        return this.storageObject[key] = value;
+    }
+    removeItem(key){
+        return delete this.storageObject[key];
+    }
+}
+
+self.window.sessionStorage = new Storage({});
+self.window.localStorage = new Storage({});
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDSUkbCZw1AjW2m7Rvj_LH-ygbKW_34Ov0",
+    authDomain: "examentrainer-bizboard.firebaseapp.com",
+    databaseURL: "https://examentrainer-bizboard.firebaseio.com",
+    storageBucket: "examentrainer-bizboard.appspot.com"
+});
 
 self.addEventListener('message', (evt)=>{
     console.log(evt);
