07ac66d38f9e4a0d8095197b85d0c60833dee103
Tom Clement <mr@tomclement.nl>
2016-06-29 17:41:52 +0200

    WIP rework of dependency injection and app bootstrapping

--- src/core/App.js
+++ src/core/App.js
@@ -7,18 +7,23 @@
 
  */
 
-
-import {inject, annotate}       from 'di';
-import {Router}                 from './Router.js';
+import FastClick                from 'fastclick';
+import Engine                   from 'famous/core/Engine.js';
 import Context                  from 'famous/core/Context.js';
+import AnimationController      from 'famous-flex/AnimationController.js';
+
+import {provide}                from '../utils/di/Decorators.js';
+import {ArvaRouter}             from '../routers/ArvaRouter.js';
+import {Injection}              from '../utils/Injection.js';
+import {Router}                 from './Router.js';
 
 import '../utils/hotfixes/Polyfills.js';
+import '../utils/hotfixes/FamousKeyboardOffset.js';
 
 /**
  * The App class exposes the Router which can be used to configure the Application's routing settings.
  * You can specify which Route should be default by calling router.setDefault(controller, method);
  */
-@inject(Router, Context)
 export class App {
 
     /**
@@ -27,9 +32,56 @@ export class App {
      * @param {Context} context The data context instance to use in the app.
      * @returns {App} App instance
      */
-    constructor(router, context) {
+    constructor() {
+        /* Options are defined as a static property on the class that extends this App */
+        let options = this.constructor.options || {};
+        
+        /* Allow user taps to emit immediately as click events,
+         * instead of having the default 300ms delay. */
+        FastClick(document.body);
+        
+        /* Add default class providers to DI engine */
+        Injection.addProviders(options.router || ArvaRouter, FamousContextSingleton, NewAnimationController);
+        
+        /* Request instances of a Router and a Famous Context. */
+        let [router, context] = Injection.getAll(Router, Context);
+        
+        /* Load controllers */
+        this.controllers = Injection.getAll(...options.controllers);
+        
+        if(this.constructor.loaded && typeof this.constructor.loaded === 'function') {
+            try { this.constructor.loaded(); } catch(error) { console.log('Caught exception in App.loaded():', error); }
+        }
+
         this.router = router;
         this.context = context;
         this.router.run();
+
+        /* Hide splash screen */
+        if(navigator && navigator.splashscreen) { navigator.splashscreen.hide(); }
+
+        if(this.done && typeof this.done === 'function') {
+            try { this.done(); } catch(error) { console.log('Caught exception in App.done():', error); }
+        }
+    }
+}
+
+@provide(Context)
+class FamousContextSingleton {
+    static famousContext = null;
+
+    constructor() {
+        return FamousContextSingleton.famousContext || (FamousContextSingleton.famousContext = Engine.createContext(null));
+    }
+}
+
+@provide(AnimationController)
+class NewAnimationController {
+    constructor() {
+        let context = new FamousContextSingleton();
+        var controller = new AnimationController();
+
+        context.add(controller);
+        return controller;
     }
 }
