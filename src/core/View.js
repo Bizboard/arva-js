9fcb0d64894efd6b4b0e9037c41a78a3c9640e70
Karl Lundfall <karl@bizboard.nl>
2016-07-14 12:33:57 +0200

    Fixed sometimes buggy origin/align for docked undefined sized things

--- src/core/View.js
+++ src/core/View.js
@@ -651,24 +651,34 @@ export class View extends FamousView {
                 let translateWithProportion = (proportion, size, translation, dimension, factor) =>
                     translation[dimension] += size[dimension] ? factor * size[dimension] * proportion[dimension] : 0;
                 translate = [...translate]; //shallow copy the translation to prevent the translation for happening multiple times
+
+                /* If no docksize was specified in a certain direction, then use the context size without margins */
+                let outerDockSize = [];
+                let {viewMargins = [0, 0, 0, 0]} = this.decorations;
+                let horizontalMargins = viewMargins[1] + viewMargins[3];
+                let verticalMargins = viewMargins[0] + viewMargins[2];
+                let sizeWithoutMargins = [context.size[0] - horizontalMargins, context.size[1] - verticalMargins];
+
+                for (let index of [0, 1]) {
+                    if (dockSizeSpecified) {
+                        outerDockSize.push(dockSize[index] === undefined ? sizeWithoutMargins[index] : dockSize[index]);
+                    }
+                    if (innerSize[index] === undefined) {
+                        innerSize[index] = sizeWithoutMargins[index];
+                    }
+                }
+                if (!dockSizeSpecified) {
+                    let dockingDirection = this._getDockType(dockMethod);
+                    outerDockSize[dockingDirection] = innerSize[dockingDirection];
+                    outerDockSize[+!dockingDirection] = sizeWithoutMargins[+!dockingDirection];
+                }
+
                 if (origin) {
                     translateWithProportion(origin, innerSize, translate, 0, -1);
                     translateWithProportion(origin, innerSize, translate, 1, -1);
                 }
                 if (align) {
-                    /* If no docksize was specified in a certain direction, then use the context size without margins */
-                    let outerDockSize = [];
-                    let {viewMargins = [0, 0, 0, 0]} = this.decorations;
-                    let horizontalMargins = viewMargins[1] + viewMargins[3];
-                    let verticalMargins = viewMargins[0] + viewMargins[2];
-                    let sizeWithoutMargins = [context.size[0] - horizontalMargins, context.size[1] - verticalMargins];
-                    if (dockSizeSpecified) {
-                        for (let [index, singleSize] of dockSize.entries()) {
-                            outerDockSize.push(singleSize === undefined ? sizeWithoutMargins[index] : singleSize);
-                        }
-                    } else {
-                        outerDockSize = sizeWithoutMargins;
-                    }
+
                     translateWithProportion(align, outerDockSize, translate, 0, 1);
                     translateWithProportion(align, outerDockSize, translate, 1, 1);
                 }
@@ -902,14 +912,16 @@ export class View extends FamousView {
 
     }
 
+    _getDockType(dockMethodToGet){
+        let dockTypes = [['right', 'left'], ['top', 'bottom']];
+        return _.findIndex(dockTypes, (dockMethods) => ~dockMethods.indexOf(dockMethodToGet));
+    }
+
     _calculateDockedRenderablesBoundingBox() {
         let {docked: dockedRenderables, filled: filledRenderables} = this._groupedRenderables;
-
         let {dockMethod} = dockedRenderables.get(dockedRenderables.keyAt(0)).decorations.dock;
-        let dockTypes = [['right', 'left'], ['top', 'bottom']];
         /* Gets the dock type where, 0 is right or left (horizontal) and 1 is top or bottom (vertical) */
-        let getDockType = (dockMethodToGet) => _.findIndex(dockTypes, (dockMethods) => ~dockMethods.indexOf(dockMethodToGet));
-        let dockType = getDockType(dockMethod);
+        let dockType = this._getDockType(dockMethod);
         let dockingDirection = dockType;
         let orthogonalDirection = !dockType + 0;
 
@@ -921,7 +933,7 @@ export class View extends FamousView {
             let {decorations} = dockedRenderable;
             let {dockMethod: otherDockMethod} = decorations.dock;
             /* If docking is done orthogonally */
-            if (getDockType(otherDockMethod) !== dockType) {
+            if (this._getDockType(otherDockMethod) !== dockType) {
                 return [NaN, NaN];
             } else {
                 /* Resolve both inner size and outer size */
