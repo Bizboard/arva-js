f394ea33ac6c3cb1ddeb2406b774239cbaa5e357
Tom Clement <mr@tomclement.nl>
2016-07-14 10:01:39 +0200

    Fixed Injection.getAll() not using new Injection.get() footprint. Added usage documentation

--- src/utils/Injection.js
+++ src/utils/Injection.js
@@ -7,23 +7,52 @@ import {Injector}                   from './di/Injector.js';
 export class Injection {
     static injector = new Injector();
 
+    /**
+     * Requests an instance of the given class from the DI engine. If an instance
+     * of that class with the same construction parameters already exists, a reference
+     * to it is returned. Otherwise a new instance is created.
+     *
+     * Example usage:
+     * get(HomeController, param1, param2);
+     *
+     * @param {Function} classConstructor The class of which an instance is wanted
+     * @param {Array} constructionParams A list of parameters to be passed to the class constructor
+     * @returns {Object} Instance of the given class
+     */
     static get(classConstructor, ...constructionParams) {
         return this.injector.get(classConstructor, constructionParams);
     }
 
     /**
+     * Requests instances of multiple classes at once.
      *
-     * @param {Array} classContructorArray Array of classes to instantiate. May also be an array where each item is an array containing
+     * Example usage:
+     * let instances = getAll(ArvaRouter, [HomeController, [param1, param2]], App);
+     *
+     * @param {Array} classContructorArray Array of classes to instantiate.
+     * May also be an array where each item is an array containing the class as
+     * its first element, and an array of parameters as its second element.
+     *
+     * @returns {Array} An array of instances of the requested classes
      */
     static getAll(...classContructorArray) {
         let results = [];
         for (let entry of classContructorArray) {
             let [constructor, params] = entry instanceof Array ? [entry[0], entry[1]] : [entry, []];
-            results.push(this.get(constructor, params));
+            results.push(this.get(constructor, ...params));
         }
         return results;
     }
 
+    /**
+     * Registers classes as the default provider for their Provide annotation (set by @provide decorator).
+     *
+     * Example usage:
+     * addProviders(ArvaRouter, FamousContext);
+     *
+     * @param {Array} classConstructors
+     * @returns {void}
+     */
     static addProviders(...classConstructors) {
         for (let constructor of classConstructors) {
             this.injector._loadFnOrClass(constructor);
