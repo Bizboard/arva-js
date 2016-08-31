1424d6d9c33a8ef39836ebe1fe470eb51e86df0f
Vladie <vlad@Bizboards-Mac-mini.local>
2016-08-31 17:13:58 +0200

    Revert "Revert "Added the draggable property to the Draggable renderable.""
    This reverts commit f1e8542976ee65a32296b6b5b196e8499903f9f3.

--- src/core/View.js
+++ src/core/View.js
@@ -1400,6 +1400,7 @@ export class View extends FamousView {
         } else if (draggableOptions) {
             renderable.node = new RenderNode();
             let draggable = new Draggable(draggableOptions);
+            renderable.draggable = draggable;
             renderable.node.add(draggable).add(renderable);
             renderable.pipe(draggable);
             renderable.pipe(this._eventOutput);
