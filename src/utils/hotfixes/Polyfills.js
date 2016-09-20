fda606b4549e6ff3f604412d6179bef640c2d8b3
Karl Lundfall <karl@bizboard.nl>
2016-09-20 12:01:20 +0200

    Added a todo to fix chrome newer versions displacing scaled renderables

--- src/utils/hotfixes/Polyfills.js
+++ src/utils/hotfixes/Polyfills.js
@@ -2,6 +2,12 @@
  * Created by tom on 06/01/16.
  */
 
+/*
+ * TODO: Find a way if possible to make chrome handle scaling correctly. There is a bug currently where chrome
+ * displaces an object when messing around with scaling
+ */
+
+
 import './polyfills/FunctionName.js';
 import './polyfills/RequestAnimationFrame.js';
 import './polyfills/StartsWith.js';
