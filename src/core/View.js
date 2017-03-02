f533be3f801d44b206c79dbcfd93f69bce808ff0
Karl Lundfall <karl@bizboard.nl>
2017-03-02 16:22:36 +0100

    Created decorator to nest things

--- src/core/View.js
+++ src/core/View.js
@@ -533,6 +533,7 @@ export class View extends FamousView {
         this.layout = new LayoutController({
             flow: !!this.decorations.useFlow || hasFlowyRenderables,
             partialFlow: !this.decorations.useFlow,
+            group: this.decorations.nested,
             nativeScroll: !!this.decorations.nativeScrollable,
             flowOptions: this.decorations.flowOptions || {spring: {period: 200}},
             layout: function (context, options) {
