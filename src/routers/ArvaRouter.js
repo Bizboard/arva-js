8d4e375e9417f1900f634572e28ec7799c3f8dd1
Karl Lundfall <karl@bizboard.nl>
2017-05-05 15:35:37 +0200

    Added check for non-existent previous route in ArvaRouter

--- src/routers/ArvaRouter.js
+++ src/routers/ArvaRouter.js
@@ -275,7 +275,9 @@ export class ArvaRouter extends Router {
     goBackInHistory() {
         /* Default behaviour: go back in history in the arva router */
         let previousRoute = this.getPreviousRoute();
-        this.go(previousRoute.controller, previousRoute.method, previousRoute.parameters || null);
+        if(previousRoute){
+            this.go(previousRoute.controller, previousRoute.method, previousRoute.parameters || null);
+        }
     }
 
     _setupNativeBackButtonListener() {
