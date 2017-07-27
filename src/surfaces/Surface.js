613d1db1c813b7f6d1e6fad824e88b48dfbf60b3
Unknown <kalle.lundfall@gmail.com>
2017-07-27 14:16:20 +0200

    Added minor documentation for surfaces/Surface.js

--- src/surfaces/Surface.js
+++ src/surfaces/Surface.js
@@ -3,4 +3,20 @@
  */
 import FamousSurface from 'famous/core/Surface.js';
 
+/**
+ * A base class for viewable content and event
+ *   targets inside an Arva application, containing a renderable document
+ *   fragment. Like an HTML div, it can accept internal markup,
+ *   properties, classes, and handle events.
+ *
+ * @class Surface
+ * @constructor
+ *
+ * @param {Object} [options] default option overrides
+ * @param {Array.Number} [options.size] [width, height] in pixels
+ * @param {Array.string} [options.classes] CSS classes to set on target div
+ * @param {Array} [options.properties] string dictionary of CSS properties to set on target div
+ * @param {Array} [options.attributes] string dictionary of HTML attributes to set on target div
+ * @param {string} [options.content] inner (HTML) content of surface
+ */
 export const Surface = FamousSurface;
